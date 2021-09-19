8119c79b3e17ce115c842ea894d81814448fe0f4
WannaDrunk <wannadrunk@gmail.com>
2021-09-20 00:18:21 +0700

    add other message type and notify as well.

--- linebot/line2telegram/README.md
+++ linebot/line2telegram/README.md
@@ -1,6 +1,9 @@
 # LINE to Telegram
 This LINE bot to forward all text messages to Telegram.
 
+## What's new
+r.0.02 202109xx
+
 ## Require
-- LINE channel access token 
-- Telegram Token and Chat ID
+- LINE channel access token and define system variable named LINE_TOKEN
+- Telegram Token and Chat ID, define system variables named TG_TOKEN and TG_CHANNEL respectively.
--- linebot/line2telegram/routes.py
+++ linebot/line2telegram/routes.py
@@ -12,6 +12,7 @@ TG_URL = f'https://api.telegram.org/bot{TG_TOKEN}/sendmessage'
 
 ## initial all variable
 discard_events = ['join', 'leave', 'memberJoined', 'memberLeft', 'follow', 'unfollow', 'leave', 'postback', 'beacon', 'accountLink', 'things', ]
+others_type = ['image', 'image', 'sticker']
 
 logging.basicConfig(filename='line2telegram.log',
                     level=logging.DEBUG, format='')
@@ -31,36 +32,25 @@ def l2tg_main():
         # discard some LINE events.
         if payload['events'][0]['type'] in discard_events:
             return '.l.', 200
-        
+
+        fwdmsg = ''
+
+        # GROUP info
+        if payload['events'][0]['source']['type'] == 'group':
+            groupid = payload['events'][0]['source']['groupId']
+            userid = payload['events'][0]['source']['userId']
+
+            fwdmsg = get_user_name(groupid, userid) + " in " + get_group_name(groupid)
+
+        # ROOM info
+        if payload['events'][0]['source']['type'] == 'room':
+            roomid = payload['events'][0]['source']['roomId']
+            userid = payload['events'][0]['source']['userId']
+
+            fwdmsg = get_user_name(roomid, userid) + " in room."
+
         # check only text type will forward to Telegram
         if payload['events'][0]['message']['type'] == 'text':
-            # Group info
-            fwdmsg = ''
-            if payload['events'][0]['source']['type'] == 'group':
-                groupid = payload['events'][0]['source']['groupId']
-                userid = payload['events'][0]['source']['userId']
-                headers = { 'Authorization': 'Bearer ' + LINE_TOKEN }
-                data = {}
-
-                url = f'https://api.line.me/v2/bot/group/{groupid}/member/{userid}'
-                r = requests.get(url, headers=headers, data=data).json()
-                fwdmsg = r["displayName"] + " in "
-
-                url = f'https://api.line.me/v2/bot/group/{groupid}/summary'
-                r = requests.get(url, headers=headers, data=data).json()
-                fwdmsg += r["groupName"]
-
-            # Room info
-            if payload['events'][0]['source']['type'] == 'room':
-                roomid = payload['events'][0]['source']['roomId']
-                userid = payload['events'][0]['source']['userId']
-                headers = { 'Authorization': 'Bearer ' + LINE_TOKEN }
-                data = {}
-
-                url = f'https://api.line.me/v2/bot/room/{roomid}/member/{userid}'
-                r = requests.get(url, headers=headers, data=data).json()
-                fwdmsg = r["displayName"] + " in room."
-
             message = "LINE: " + fwdmsg + "\n\n"
             message += payload['events'][0]['message']['text']
 
@@ -72,4 +62,36 @@ def l2tg_main():
             response = requests.get(TG_URL, headers=headers, data=data)
             logging.info(response.text)
 
+        # Other message type
+        if payload['events'][0]['message']['type'] in others_type:
+            message = "LINE:" + fwdmsg + " sent " + payload['events'][0]['message']['type']
+
+            headers = {}
+            data = {
+                "text": message,
+                "chat_id": TG_CHANNEL
+            }
+
+            response = requests.get(TG_URL, headers=headers, data=data)
+            logging.info(response.text)
+            
     return '', 200
+
+def get_group_name(source_type):
+    headers = { 'Authorization': 'Bearer ' + LINE_TOKEN }
+    data = {}
+
+    url = f'https://api.line.me/v2/bot/group/{source_type}/summary'
+    r = requests.get(url, headers=headers, data=data).json()
+
+    return r["groupName"]
+
+def get_user_name(source_type, userid):
+    headers = { 'Authorization': 'Bearer ' + LINE_TOKEN }
+    data = {}
+
+    url = f'https://api.line.me/v2/bot/room/{source_type}/member/{userid}'
+    r = requests.get(url, headers=headers, data=data).json()
+
+    return r["displayName"]
+
